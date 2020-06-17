4a9762e2962bd797ba696dc76cd02f9e70495690
Coulomb <coulombleo@gmail.com>
2020-06-17 15:55:53 +0200

    img

--- src/Modele/Entite/PacMan.java
+++ src/Modele/Entite/PacMan.java
@@ -21,21 +21,13 @@ import java.util.List;
  * @author Epulapp
  */
 public class PacMan extends Entite {
-    private Image imPacman = new Image(Configuration.PATH_TO_IMG + "Pacman.png");
+    private Image imPacman = new Image(Configuration.PATH_TO_IMG + "/Sprite/TestAnim/pm1.png");
 
     private boolean animation = false;
     public List<Image> images = new ArrayList<>();
 
     public Image getImPacman() {
         synchronized(this){
-            
-//            System.out.println("changement pacman");
-            animation = !animation;
-//            if (animation) {
-//                imPacman = new Image(Configuration.PATH_TO_IMG + "Pacman.png");
-//            }else{
-//                imPacman = new Image(Configuration.PATH_TO_IMG + "PacmanFerme.png");
-//            }
             return imPacman;
         }
     }
@@ -49,12 +41,6 @@ public class PacMan extends Entite {
                         Thread.sleep(100);
                     } catch (InterruptedException ex) {
                         Logger.getLogger(PacMan.class.getName()).log(Level.SEVERE, null, ex);
-                    }
-                    animation = !animation;
-                    if (animation) {
-                        imPacman = new Image(Configuration.PATH_TO_IMG + "Pacman.png");
-                    }else{
-                        imPacman = new Image(Configuration.PATH_TO_IMG + "PacmanFerme.png");
                     }
                 }
             }
